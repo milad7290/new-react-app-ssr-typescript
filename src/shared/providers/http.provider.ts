01601bd14c387b3ad3b40e73a8657a3f69f1e862
Soroosh Ansarimehr <sorosh.ansari@gmail.com>
2020-05-25 18:02:20 +0800

    http service added

--- src/shared/providers/http.provider.ts
+++ src/shared/providers/http.provider.ts
@@ -1,90 +1,46 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from 'axios';
 
-export function HttpProvider({
-  base = "https://jsonplaceholder.typicode.com",
-  url,
-  data,
-  headers = {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    // Authorization: `Bearer ${getAccessToken()}`,
-    // ClientType: "Web",
-  },
-  method = HttpMethod.GET,
-}: HttpProviderType) {
-  const fullUrl = url.startsWith("/") ? `${base}${url}` : `${base}/${url}`;
-
-  const options: HttpProviderOptionType = {
-    method,
-    headers,
+export async function HttpProvider<T>({
+    base = 'https://jsonplaceholder.typicode.com',
+    url,
     data,
-  };
-
-  return new Promise(async (resolve) => {
-    try {
-      const result = await axios(fullUrl, options);
-      const response = await result.data;
-
-      return resolve(resultHandler({ result: response }));
-    } catch (error) {
-      console.log(`Failed response for ${url}: ${error}`);
-
-      return resolve(resultHandler({ result: error }));
+    params,
+    method,
+}: {
+    base?: string;
+    url: string;
+    data?: any;
+    params: any;
+    method: HttpMethod;
+}): Promise<T> {
+    const fullUrl = url.startsWith('/') ? `${base}${url}` : `${base}/${url}`;
+    const config: AxiosRequestConfig = {
+        method,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            // Authorization: `Bearer ${getAccessToken()}`,
+            // ClientType: "Web",
+        },
+    };
+    if (params) {
+        config.params = params;
     }
-  });
-}
-
-export function resultHandler({ result }: any) {
-  const { message } = result || {};
-
-  switch (true) {
-    case "message" in result || "stack" in result:
-      return {
-        result: null,
-        error: message,
-        errorCode: null,
-        type: FetchResultType.FetchError,
-      };
-    // case !isSuccess:
-    //   return {
-    //     result: null,
-    //     error: error.displayMessage,
-    //     errorCode: error.errorCode || null,
-    //     type: FetchResultType.ServerError,
-    //   };
-    default:
-      return {
-        result,
-        error: null,
-        errorCode: null,
-        type: FetchResultType.Success,
-      };
-  }
-}
-
-export interface HttpProviderType {
-  base?: string;
-  url: string;
-  data?: any;
-  headers?: any;
-  method?: string;
-}
-
-export interface HttpProviderOptionType {
-  data: any;
-  headers: any;
-  method: any;
+    if (data) {
+        config.data = data;
+    }
+    try {
+        return (await axios(fullUrl, config)).data;
+    } catch (error) {
+        console.log(`Failed response for ${url}: ${error}`);
+        throw error;
+    }
 }
 
 export const FetchResultType = {
-  ServerError: "server_error",
-  FetchError: "fetch_error",
-  Success: "success",
+    ServerError: 'server_error',
+    FetchError: 'fetch_error',
+    Success: 'success',
 };
 
-export const HttpMethod = {
-  POST: "POST",
-  GET: "GET",
-  PUT: "PUT",
-  DELETE: "DELETE",
-};
+export type HttpMethod = 'POST' | 'GET' | 'PUT' | 'DELETE';
