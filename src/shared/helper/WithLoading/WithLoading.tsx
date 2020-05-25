d6334aa5eb2003ae1a4934343a2524f6bc53663f
milad <sakhaei90.itsu@gmail.com>
2020-05-25 12:35:28 +0430

    some change

--- src/shared/helper/WithLoading/WithLoading.tsx
+++ src/shared/helper/WithLoading/WithLoading.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import './WithLoading.scss';
+import css from './WithLoading.module.css';
 
 const WithLoading = (Component: any) => {
     return function WihLoadingComponent({ isLoading, ...props }: any) {
         if (!isLoading) {
             return <Component {...props} />;
         }
-        return <p className="loading">بارگذاری...</p>;
+        return <p className={css.loading}>بارگذاری...</p>;
     };
 };
 export default WithLoading;
